aa4bfd6ef0a86e83fe319f3e90d7ca89722f2e7c
James-Roff <77630845+James-Roff@users.noreply.github.com>
2021-01-18 21:13:43 +0000

    Completed 2
    Coded the solution to problem 2

--- ques_2.py
+++ ques_2.py
@@ -1,9 +1,26 @@
 from ques_base import *
 
 class question_2(question_base):
+    maximum = 4000000
+
     def __init__(self):
         self.statement = "Even Fibonacci numbers"
         self.difficulty = 0.05
 
     def solve(self):
-        return 1
+        ans = 0
+
+        prev = [1, 2]
+        for num in prev:
+            if num % 2 == 0:
+                ans += num
+
+        next_num = prev[0] + prev[1]
+        while next_num <= self.maximum:
+            if next_num % 2 == 0:
+                ans += next_num
+            prev[0] = prev[1]
+            prev[1] = next_num
+            next_num = prev[0] + prev[1]
+
+        return ans
