c30c334da13f579a8bdbb7b0bfe87685345301bd
Eatough <46674597+Eatough@users.noreply.github.com>
2021-02-16 17:53:59 +0000

    Problem 87, every little counts

---
+++ ques_87.py
@@ -0,0 +1,35 @@
+from ques_base import *
+
+class question_87(question_base):
+
+    def __init__(self):
+        self.statement = "Prime power triples"
+        self.difficulty = 0.20
+
+    def solve(self):
+        large = 50000000  # We want to find numbers up to 50 million
+        primes = primes_in_range(1, int(math.sqrt(large)))  # List of primes up to sqrt(n)
+        primes_c = list_upto(primes, int(large ** (1 / 4)))  # subset of primes up to cube root n
+        primes_b = list_upto(primes, int(large ** (1 / 3)))  # subset of primes up to 4th root n
+        primes_a = primes
+        list_of_n = []  # list to store all numbers made up of a^2 + b^3 + d^4
+
+        for c in primes_c:
+            for b in primes_b:
+                for a in primes_a:
+                    n = a * a + b * b * b + c * c * c * c
+                    if n < large:
+                        list_of_n.append(n)
+                    else:  # if current value of c^4 + b^3 + a^2 is too big,
+                        break  # then so will all other 'n' with this combination of c and b and any larger a
+
+        list_of_n.sort()  # Sort list numerically increasing
+        new_list = []  # This new list will not contain duplicates
+
+        prev_n = 0
+        for n in list_of_n:
+            if n != prev_n:
+                new_list.append(n)
+            prev_n = n
+
+        return len(new_list)
